1d5afdace5a0e27166f6699e7babc30b932f987a
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-02-05 22:49:02 +0100

    created init function

--- index.js
+++ index.js
@@ -5,16 +5,67 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a short description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please explain how to use your application.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GNU', 'Apache'],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide your contribution guidelines.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide instructions for testing the project.',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+        .then(answers => {
+            ;
+            const filename = './output/README.md';
+            writeToFile(filename, generateMarkdown(answers));
+        })
 }
 
 // function call to initialize program
